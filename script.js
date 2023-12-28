f9f885455c121f9c08197da404d31018288cb688
campsking <36866302+campsking@users.noreply.github.com>
2023-12-28 12:24:54 -0400

    test_6

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let secciones = document.querySelectorAll('.seccion');
 let indiceSeccionActual = 0;
 let startX, startY, isDragging = false;
-const umbral = 10; // Umbral para diferenciar entre desplazamiento vertical y horizontal
+const umbralVertical = 50; // Ajusta este valor para controlar la sensibilidad del desplazamiento vertical
 
 // Función para cambiar la sección activa
 function cambiarSeccionActiva(nuevoIndice) {
@@ -29,15 +29,14 @@ window.addEventListener('touchmove', (event) => {
     let deltaX = event.touches[0].pageX - startX;
     let deltaY = event.touches[0].pageY - startY;
 
-    // Verificar si el desplazamiento es principalmente horizontal o vertical
-    if (Math.abs(deltaX) > Math.abs(deltaY) && Math.abs(deltaX) > umbral) {
-        // Desplazamiento horizontal: manejar la galería
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+        // Desplazamiento horizontal en la galería
         if (event.target.closest('.galeria')) {
             event.preventDefault(); // Prevenir el desplazamiento vertical
-            // Aquí tu lógica para desplazar horizontalmente la galería
+            // Código para manejar el arrastre horizontal...
         }
-    } else if (Math.abs(deltaY) > umbral) {
-        // Desplazamiento vertical: manejar el cambio de sección
+    } else if (Math.abs(deltaY) > umbralVertical) {
+        // Desplazamiento vertical: cambiar de sección
         cambiarSeccionActiva(deltaY > 0 ? indiceSeccionActual - 1 : indiceSeccionActual + 1);
     }
 }, false);
@@ -46,4 +45,22 @@ window.addEventListener('touchend', () => {
     isDragging = false;
 }, false);
 
-// Resto de tu lógica para la galería...
+// Agregar eventos para arrastre en la galería en PC
+const galeria = document.querySelector('.galeria');
+galeria.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    startX = e.pageX - galeria.offsetLeft;
+    scrollLeft = galeria.scrollLeft;
+});
+
+window.addEventListener('mouseup', () => {
+    isDragging = false;
+});
+
+galeria.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - galeria.offsetLeft;
+    const walk = (x - startX) * 2; // Multiplicador de velocidad de arrastre
+    galeria.scrollLeft = scrollLeft - walk;
+});
